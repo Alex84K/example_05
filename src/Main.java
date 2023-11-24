c11bc138d28b16c3db3f4a184ebb46c1831be473
Alex Kovalov <isaev.msksim84@gmail.com>
2023-11-24 11:58:06 +0100

    init hallo new

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    // создалми удалённый репо
+    // создали локальный репо  и вводим команды
+    // git init
+    // git ignore
+    // git remote add origin git@github.com................
+    // git add .
+    // git push origin main
 }
